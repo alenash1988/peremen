e99889ae8ec443c9c16bba05e28293ee1972a91e
alenash1988 <Aleonash1988@mail.ru>
2024-08-15 17:42:47 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+Total_homwork = 12
+Total_hors = 1.5
+name = "Python"
+Time_for_one_task = 1.5 / 12
+print(name, "всего задач:",Total_homwork, "затраченно часов:",Total_hors, "среднее время выполнения:",Time_for_one_task)
