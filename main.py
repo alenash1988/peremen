ce8bb7244c26d47958ca23e24e0ca686aa09b7dd
alenash1988 <Aleonash1988@mail.ru>
2024-08-15 17:56:11 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 Total_homwork = 12
 Total_hors = 1.5
 name = "Python"
-Time_for_one_task = 1.5 / 12
+Time_for_one_task = Total_hors/Total_homwork
 print(name, "всего задач:",Total_homwork, "затраченно часов:",Total_hors, "среднее время выполнения:",Time_for_one_task)
